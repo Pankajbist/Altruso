f8265d160eef9ce0b2c384b99ebb9037168f0a00
Pankajbist <pankajbist000@gmail.com>
2023-08-01 18:13:33 +0545

    avatar added

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,163 +1,95 @@
-import { Avatar, Space, Popover } from 'antd';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-import CustomLogo from './logo';
+import { Avatar, Drawer, Button } from 'antd';
+import { LogoutOutlined, UserOutlined } from '@ant-design/icons'; // Import icons
+import { CustomLogo } from './logo';
+import Image from 'next/image';
+import Logo from '../../public/Altruso-logo.png';
 
-function NavBar() {
+export default function Header() {
+  const [isDrawerVisible, setDrawerVisible] = useState(false);
   const router = useRouter();
-  const { isLoggedIn, userDetails } = useSelector(state => state.users);
 
   const handleLogout = () => {
-    router.push('/profile');
+    // Get the value of isLoggedIn from useSelector
+    const { isLoggedIn } = useSelector((state) => state.users);
+
+    // If the user is logged in, redirect to "http://localhost:3000/profile"
+    if (isLoggedIn) {
+      router.push('http://localhost:3000/profile');
+    }
+
+    // Perform any necessary logout actions here, such as clearing authentication tokens or user data from the state.
   };
 
-  const content = (
-    <div>
-      <Link href="/profile"legacyBehavior>
-        <a>Profile</a>
-      </Link>
-      <p onClick={handleLogout}>Logout</p>
-    </div>
-  );
+  const { isLoggedIn, userDetails } = useSelector((state) => state.users);
+
+  const showDrawer = () => {
+    setDrawerVisible(true);
+  };
+
+  const hideDrawer = () => {
+    setDrawerVisible(false);
+  };
 
   return (
-    <header>
-      <nav className="navbar">
-        <div className="nav-container">
-          <div className="logo">
-            <Link href="/"legacyBehavior>
-              <a>
-                <CustomLogo />
-              </a>
-            </Link>
-          </div>
+    <nav className="navbar">
+      <a className="text-xl font-semibold" href="/">
+        <Image src={Logo} alt="Logo" width={200} height={100} />
+      </a>
 
-          {isLoggedIn ? (
-            <div>
-              <Popover
-                placement="bottom"
-                title={userDetails.fullName}
-                content={content}
-                trigger="click"
-                overlayClassName="avatar-popover" // Apply specific styling for Popover
-              >
-                <Avatar size="large" className="user-avatar">
-                  {userDetails.fullName[0]}
-                </Avatar>
-              </Popover>
-            </div>
-          ) : (
-            <ul className="nav-menus"legacyBehavior>
-              <li>
-                <Link href="/login"legacyBehavior>
-                  <a>Login</a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/register"legacyBehavior>
-                  <a className="active">Signup</a>
-                </Link>
-              </li>
-            </ul>
-          )}
+      {isLoggedIn ? (
+        <div>
+          <a  onMouseEnter={showDrawer} >
+            <Avatar
+              size="large"
+              style={{
+                backgroundColor: 'aqua',
+                color: '#f56a00',
+                marginTop: '3px',
+                fontSize: '1.5rem',
+                marginRight: '-5px',
+                textAlign: 'center',
+              }}
+            >
+              {userDetails.fullName[0]}
+            </Avatar>
+          </a>
+
+          <Drawer      title=
+      
+          <b style={{  fontSize: '1.5rem' }}>{"Edit_Your_Profile"}</b>
+    
+
+            placement="right"
+            closable={true}
+            onClose={hideDrawer}
+            
+            visible={isDrawerVisible}
+            width={300}
+          >
+           <b style={{ color: 'aqua', fontSize: '1.5rem' }}>{` ${userDetails.fullName}`}</b>
+            <p><b>Settings & Privacy</b></p>
+        <p><b>Help & Support</b></p>
+        <p><b> Give Feedback</b></p>
+        <p><b> Display & Accessibility</b></p>
+        <p><b> LogOut</b></p>
+          </Drawer>
         </div>
-      </nav>
-      <style jsx>{`
-        /* Navbar container */
-        .nav-container {
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-          padding: 1rem;
-          background-color: #f0f0f0;
-        }
-
-        /* Logo */
-        .logo a {
-          text-decoration: none;
-          color: #000;
-          font-weight: bold;
-          font-size: 1.5rem;
-        }
-
-        /* Logo image */
-        .logo img {
-          width: 40px;
-          height: 40px;
-        }
-
-        /* Nav menus for logged-out users */
-        .nav-menus {
-          list-style: none;
-          display: flex;
-        }
-
-        .nav-menus li {
-          margin-right: 1rem;
-        }
-
-        .nav-menus a {
-          text-decoration: none;
-          color: #000;
-          padding: 0.5rem 1rem;
-          border-radius: 5px;
-        }
-
-        /* Active menu item */
-        .nav-menus a.active {
-          background-color: #007bff;
-          color: #fff;
-        }
-
-        /* User Avatar */
-        .user-avatar {
-          cursor: pointer;
-        }
-
-        /* Popover content */
-        .avatar-popover {
-          padding: 1rem;
-        }
-
-        .avatar-popover a {
-          display: block;
-          margin-bottom: 0.5rem;
-          color: #000;
-          text-decoration: none;
-        }
-
-        .avatar-popover p {
-          cursor: pointer;
-          color: #007bff;
-          margin: 0;
-        }
-
-        /* Responsive design */
-        @media (max-width: 768px) {
-          .nav-container {
-            flex-direction: column;
-          }
-
-          .logo {
-            margin-bottom: 1rem;
-          }
-
-          .nav-menus {
-            margin-top: 1rem;
-          }
-
-          .nav-menus li {
-            margin-right: 0.5rem;
-          }
-
-          .user-avatar {
-            margin-top: 1rem;
-          }
-        }
-      `}</style>
-    </header>
+      ) : (
+        <ul className="nav-menus">
+          <li>
+            <a href="/login">Login</a>
+          </li>
+          <li>
+            <a href="/register" className="active">
+              Signup
+            </a>
+          </li>
+        </ul>
+      )}
+    </nav>
   );
 }
-
-export default NavBar;
