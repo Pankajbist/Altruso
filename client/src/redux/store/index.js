c5662eaafd590c858653c9b8675875a148091b4f
Pankajbist <pankajbist000@gmail.com>
2023-08-02 09:32:05 +0545

    redux setup for session storage (persist page setup) after signup or login

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,12 +1,20 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import logger from 'redux-logger'
 import users from '../reducerSlice/users'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
 const reducer = combineReducers({
     users,
 })
-const store = configureStore({
-    reducer,
+const persistedReducer = persistReducer(persistConfig, reducer)
+
+export const store = configureStore({
+    reducer:persistedReducer,
     middleware:[logger]
 })
-
-export default store;
+export const persistor = persistStore(store)
